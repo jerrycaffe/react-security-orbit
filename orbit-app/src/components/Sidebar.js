dcccf990a2307a4c7f5d9e787647991e7d53b6a0
jerrycaffe@gmail.com <jerrycaffe@gmail.com>
2021-05-02 23:53:39 +0100

    fixed authentication

--- orbit-app/src/components/Sidebar.js
+++ orbit-app/src/components/Sidebar.js
@@ -1,52 +1,61 @@
-import React from 'react';
-import classNames from 'classnames';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useContext } from "react";
+import classNames from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChartLine,
   faAddressCard,
   faChartPie,
   faCogs,
-  faDoorOpen
-} from '@fortawesome/free-solid-svg-icons';
-import { Link, useLocation } from 'react-router-dom';
-import logo from './../images/logo.png';
+  faDoorOpen,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link, useLocation } from "react-router-dom";
+import logo from "./../images/logo.png";
+import { AuthContext } from "../context/AuthContext";
 
 const navItems = [
   {
-    label: 'Dashboard',
-    path: 'dashboard',
-    icon: faChartLine
+    label: "Dashboard",
+    path: "dashboard",
+    icon: faChartLine,
+    allowedRoles: ["user", "admin"],
   },
   {
-    label: 'Inventory',
-    path: 'inventory',
-    icon: faChartPie
+    label: "Inventory",
+    path: "inventory",
+    icon: faChartPie,
+    icon: faChartLine,
+    allowedRoles: ["admin"],
   },
   {
-    label: 'Account',
-    path: 'account',
-    icon: faAddressCard
+    label: "Account",
+    path: "account",
+    icon: faAddressCard,
+    icon: faChartLine,
+    allowedRoles: ["user", "admin"],
   },
   {
-    label: 'Settings',
-    path: 'settings',
-    icon: faCogs
+    label: "Settings",
+    path: "settings",
+    icon: faCogs,
+    icon: faChartLine,
+    allowedRoles: ["user", "admin"],
   },
   {
-    label: 'Users',
-    path: 'users',
-    icon: faDoorOpen
-  }
+    label: "Users",
+    path: "users",
+    icon: faDoorOpen,
+    icon: faChartLine,
+    allowedRoles: ["admin"],
+  },
 ];
 
 const NavItem = ({ navItem }) => {
   const location = useLocation();
-  const isCurrentRoute =
-    location.pathname === `/${navItem.path}`;
+  const isCurrentRoute = location.pathname === `/${navItem.path}`;
   const classes = classNames({
-    'px-2 sm:px-6 justify-center sm:justify-start py-3 rounded-full flex': true,
-    'text-gray-600 hover:text-blue-500 transform hover:translate-x-1 transition ease-in-out duration-100': !isCurrentRoute,
-    'bg-gradient text-gray-100 shadow-lg': isCurrentRoute
+    "px-2 sm:px-6 justify-center sm:justify-start py-3 rounded-full flex": true,
+    "text-gray-600 hover:text-blue-500 transform hover:translate-x-1 transition ease-in-out duration-100": !isCurrentRoute,
+    "bg-gradient text-gray-100 shadow-lg": isCurrentRoute,
   });
   return (
     <Link to={navItem.path} className={classes}>
@@ -54,19 +63,18 @@ const NavItem = ({ navItem }) => {
         <div className="mr-0 sm:mr-4">
           <FontAwesomeIcon icon={navItem.icon} />
         </div>
-        <span className="hidden sm:block">
-          {navItem.label}
-        </span>
+        <span className="hidden sm:block">{navItem.label}</span>
       </div>
     </Link>
   );
 };
 
-const NavItemContainer = ({ children }) => (
-  <div>{children}</div>
-);
+const NavItemContainer = ({ children }) => <div>{children}</div>;
+// get user role from the auth context
 
 const Sidebar = () => {
+  const authContext = useContext(AuthContext);
+  const { role } = authContext.authState.userInfo;
   return (
     <section className="h-screen">
       <div className="w-16 sm:w-24 m-auto">
@@ -74,9 +82,14 @@ const Sidebar = () => {
       </div>
       <div className="mt-20">
         {navItems.map((navItem, i) => (
-          <NavItemContainer key={i}>
-            <NavItem navItem={navItem} />
-          </NavItemContainer>
+          <>
+            {/* check the role to render sidebar according to the user roles */}
+            {authContext.isAdmin() && navItem.allowedRoles.includes(role) && (
+              <NavItemContainer key={i}>
+                <NavItem navItem={navItem} />
+              </NavItemContainer>
+            )}
+          </>
         ))}
       </div>
     </section>
--- orbit-app/src/context/AuthContext.js
+++ orbit-app/src/context/AuthContext.js
@@ -1,17 +1,68 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from "react";
+import { useHistory } from "react-router";
 
 const AuthContext = createContext();
 const { Provider } = AuthContext;
 
 const AuthProvider = ({ children }) => {
+  const token = localStorage.getItem("token");
+  const userInfo = localStorage.getItem("userInfo");
+  const expiresAt = localStorage.getItem("expiresAt");
+
+  const history = useHistory();
+
+  const [authState, setAuthState] = useState({
+    token,
+    expiresAt,
+    userInfo: userInfo ? JSON.parse(userInfo) : {},
+  });
+  const setAuthInfo = ({ token, userInfo, expiresAt }) => {
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    localStorage.setItem("token", token);
+    localStorage.setItem("expiresAt", expiresAt);
+    setAuthState({
+      token,
+      userInfo: userInfo ? JSON.parse(userInfo) : {},
+      expiresAt,
+    });
+  };
+
+  // LOGOUT FUNCTION
+  const logout = () => {
+    // when logging out set all the info to nothing
+
+    localStorage.removeItem("userInfo");
+    localStorage.removeItem("token");
+    localStorage.removeItem("expiresAt");
+    // set Auth state to nothing
+
+    setAuthState({ token: null, expiresAt: null, userInfo: {} });
+    // redirect the user to loging with useHistory
+
+    history.push("/login");
+  };
+
+  // CHECK ADMIN ROLE
+  const isAdmin = () => {
+    return authState.userInfo.role === "admin";
+  };
+
+  // CHeck if the user is authenticated
+  const isAuthenticated = () => {
+    if (!authState.token || !authState.expiresAt) {
+      return false;
+    }
+    return new Date().getTime() / 1000 < authState.expiresAt;
+  };
+
   return (
     <Provider
       value={{
-        authState: {
-          token: null,
-          expiresAt: null,
-          userInfo: {}
-        }
+        authState,
+        setAuthState: (authInfo) => setAuthInfo(authInfo),
+        isAuthenticated,
+        logout,
+        isAdmin,
       }}
     >
       {children}
